f4a85c11f64eac06eae153586db6c23c140670b0
Felix Bonillo <felixbdev@gmail.com>
2025-07-06 13:10:47 -0400

    Correcion, al llamar a express antes de declarar

--- server.js
+++ server.js
@@ -1,45 +1,43 @@
 // Importar modulo express
 
-const express = require('express');
-const patch = require('node:path');
-const tasksRouter = require('./routes/task');
-const cors = require('cors');
+const express = require("express");
+const patch = require("node:path");
+const tasksRouter = require("./routes/task");
+const cors = require("cors");
 
 //Configuracion de cors
 
 const corsOptions = {
-  origin: 'https://taskmanagerfull.netlify.app/', // Solo permitir solicitudes desde este origen
-  methods: 'GET, POST, DELETE, PATCH', // Métodos permitidos
+  origin: "https://taskmanagerfull.netlify.app/", // Solo permitir solicitudes desde este origen
+  methods: "GET, POST, DELETE, PATCH", // Métodos permitidos
   credentials: true, // Permitir credenciales (cookies, autenticación HTTP)
-  optionsSuccessStatus: 204 // Para navegadores antiguos que no manejan bien el código de estado 204
+  optionsSuccessStatus: 204, // Para navegadores antiguos que no manejan bien el código de estado 204
 };
 
-app.use(cors(corsOptions)); // Aplicar CORS a todas las rutas
-
-
 const app = express();
+
+app.use(cors(corsOptions)); // Aplicar CORS a todas las rutas
 const PORT = process.env.PORT || 3000;
 
-// Middleware globales 
+// Middleware globales
 
 app.use(express.json()); // Para parsear el cuerpo de las solicitudes como JSON
-app.use(express.static(patch.join(__dirname, 'public'))); // Servir archivos estáticos desde la carpeta 'public' (HTML, CSS, JS) del frontend
+app.use(express.static(patch.join(__dirname, "public"))); // Servir archivos estáticos desde la carpeta 'public' (HTML, CSS, JS) del frontend
 
 //Configurar rutas definidas en taskRouter seran prefijadas por '/api/tasks'
-app.use('/api/tasks', tasksRouter);
+app.use("/api/tasks", tasksRouter);
 
 // --- Manejo de la ruta principal para servir index.html ---
 //Si se va a la raiz del servidor, enviamos el archivo index.html
 
-app.get('/', (req, res) => {
-  res.sendFile(patch.join(__dirname, 'public', 'index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(patch.join(__dirname, "public", "index.html"));
 });
-
 
 // Iniciar el servidor
 
 app.listen(PORT, () => {
   console.log(`Servidor escuchando en el puerto ${PORT}`);
   console.log(`Listo para construir el gestor de tareas con Express.js`);
-  console.log(`http://localhost:${PORT}`); 
+  console.log(`http://localhost:${PORT}`);
 });
