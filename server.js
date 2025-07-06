39d23ca14f80fe3098745937958297233bc206d1
Felix Bonillo <felixbdev@gmail.com>
2025-07-06 13:29:26 -0400

    Correcion de / en url en la integracion de los cors netlify

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 // Importar modulo express
 
 const express = require("express");
-const patch = require("node:path");
+const path = require("node:path");
 const tasksRouter = require("./routes/task");
 const cors = require("cors");
 
 //Configuracion de cors
 
 const corsOptions = {
-  origin: "https://taskmanagerfull.netlify.app/", // Solo permitir solicitudes desde este origen
+  origin: "https://taskmanagerfull.netlify.app", // Solo permitir solicitudes desde este origen
   methods: "GET, POST, DELETE, PATCH", // Métodos permitidos
   credentials: true, // Permitir credenciales (cookies, autenticación HTTP)
   optionsSuccessStatus: 204, // Para navegadores antiguos que no manejan bien el código de estado 204
@@ -22,7 +22,7 @@ const PORT = process.env.PORT || 3000;
 // Middleware globales
 
 app.use(express.json()); // Para parsear el cuerpo de las solicitudes como JSON
-app.use(express.static(patch.join(__dirname, "public"))); // Servir archivos estáticos desde la carpeta 'public' (HTML, CSS, JS) del frontend
+app.use(express.static(path.join(__dirname, "public"))); // Servir archivos estáticos desde la carpeta 'public' (HTML, CSS, JS) del frontend
 
 //Configurar rutas definidas en taskRouter seran prefijadas por '/api/tasks'
 app.use("/api/tasks", tasksRouter);
